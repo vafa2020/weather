725ddc4216f3a3673088ce21e4c986785cd75c62
Mohammad Hossein Vafaee <vafaee.mh@gmail.com>
2021-10-18 11:21:51 +0330

    added style to city component with Tailwind css

--- src/component/City.js
+++ src/component/City.js
@@ -3,8 +3,18 @@ const City = () => {
   return (
     <div className="flex flex-col items-center">
       <span>
-        <FaCloudShowersHeavy className="fill-current text-blue-600 text-9xl mt-20" />
+        <FaCloudShowersHeavy className="fill-current text-blue-600 text-9xl mt-10" />
       </span>
+      <h3 className=" mt-10 text-4xl font-serif">Find Weather Of Your City</h3>
+      <div className="flex items-center mt-20 relative">
+        <input
+          className="border-2 px-5 py-3 rounded-lg w-96"
+          placeholder="Your City ..."
+        />
+        <button className="bg-black pt-2 pb-3 px-5 rounded-lg text-white absolute bottom-1 right-1">
+          search
+        </button>
+      </div>
     </div>
   );
 };
